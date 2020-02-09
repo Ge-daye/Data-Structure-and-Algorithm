149624b99da01386c50c6073d3fbeecb57defced
Ge-daye <1815550182@qq.com>
2020-02-09 16:38:38 +0800

    插入排序第二次更新

--- src/排序/Insertion.java
+++ src/排序/Insertion.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public class Insertion {
     public static void sort(int a[]){
         int n = a.length;
+        //外层循环是准备插入第几个元素，默认从第二个开始
         for (int i = 1; i <n ; i++) {
+            //内层循环是将待插入元素与已排序的元素依次比较（倒序），如果更小就交换位置
             for (int j = i; j >0 ; j--) {
                 if(a[j]<a[j-1])
                 {
