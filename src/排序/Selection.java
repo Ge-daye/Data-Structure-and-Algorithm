96022f6d08da42ed20c02e0342ac9d04b63be00d
Ge-daye <1815550182@qq.com>
2020-02-09 16:38:11 +0800

    选择排序第二次更新

--- src/排序/Selection.java
+++ src/排序/Selection.java
@@ -7,9 +7,11 @@ public class Selection {
         int n = a.length;
         int key;//记录每次排序最小的数
         int flag;//记录最小值的索引
+        //外层循环是排序的趟数，每一趟都找到一个”最小的数“
         for (int i = 0; i < n-1; i++) {
             key = a[i];
             flag = i;
+            //内层循环是将假定最小的数与后面的数进行依次比较,更新最小值，直到最后一个，找到真正的最小值
             for (int j = i+1; j < n; j++) {
                 if(a[j]<key)
                 {
@@ -17,6 +19,7 @@ public class Selection {
                     flag = j;
                 }
             }
+            //将最小的值与假定的最小值（a[i]）交换
             a[flag] = a[i];//交换位置
             a[i] = key;
 
