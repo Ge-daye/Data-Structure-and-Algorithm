3906ee424c63e2fab4c00f19841ee19990cee3cf
Ge-daye <1815550182@qq.com>
2020-02-11 15:14:05 +0800

    希尔排序API设计

---
+++ src/排序/Shell2.java
@@ -0,0 +1,45 @@
+package 排序;
+
+import java.util.Arrays;
+//希尔排序，API设计
+public class Shell2 {
+    public static void main(String[] args) {
+        Integer arr[] = new Integer[]{9,1,2,5,7,4,12,8,6,3,5};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    //希尔排序的函数体
+    public static  void sort(Comparable a[]){
+        int n = a.length;
+        int h = a.length/2;
+        while(h>=1){
+            for (int i = h; i < n; i++) {
+                for (int j = i; j >=h; j = j-h) {
+                    if(great(a[j-h],a[j]))
+                    {
+                        exch(a,j-h,j);
+                    }
+                }
+            }
+            h = h/2;
+        }
+
+
+    }
+    //判断x是否大于v
+    public static boolean great(Comparable x,Comparable v){
+        if(x.compareTo(v)>0)
+        {
+            return true;
+        }
+        return false;
+    }
+    //交换Comparable实现类对象数组的第 i和j元素
+    public static void exch(Comparable a[],int i,int j){
+        Comparable temp  = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+}
